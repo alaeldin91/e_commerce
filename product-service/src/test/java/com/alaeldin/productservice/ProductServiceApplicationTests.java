6717ff9a4d3422e2be60a45ff9a3c51de8e83db5
Alaeldinmusa91 <alaeldinmusa91@gmail.com>
2024-01-13 15:09:47 -0800

    microService product api

---
+++ product-service/src/test/java/com/alaeldin/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaeldin.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
